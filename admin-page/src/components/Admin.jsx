e0d64b17cf26e0e7c190fdef5c53cb9c41776694
H82M19\Student <Student>
2025-04-02 19:30:58 +0700

    Layout+GridCss

---
+++ admin-page/src/components/Admin.jsx
@@ -0,0 +1,25 @@
+function Admin() {
+    return ( 
+        <div class="container">
+            <div class="header">
+                <p>My header</p>
+            </div>
+            <div class="menu">
+                <a href="#">Link 1</a>
+                <br />
+                <a href="#">Link 2</a>
+                <br />
+                <a href="#">Link 3</a>
+            </div>
+            <div class="overview">
+                <p>Overview</p>
+            </div>
+            <div class="content">
+                <h3>Lorem Ipsum</h3>
+            </div>
+
+        </div>
+     );
+}
+
+export default Admin;
