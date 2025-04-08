2d7eff58b610f05884700223a25079624ae8267b
SANG <vansang12cb5@gmail.com>
2025-04-08 13:55:24 +0700

    React Router ()

--- admin-page/src/App.jsx
+++ admin-page/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Admin from './components/Admin'
+import Projects from './components/Projects'
+import Teams from './components/Teams'
+import Analytics from './components/Analytics'
+import Messages from './components/Messages'
+import Integreations from './components/Integrations'
 function App() {
 
 
   return (
     <>
-      <Admin />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Admin />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integration" element={<Integreations />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- admin-page/src/components/Teams.jsx
+++ admin-page/src/components/Teams.jsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
-
-function Admin() {
+function Teams() {
     const [overviewData, setOverviewData] = useState({
         turnover: 0,
         profit: 0,
         newCustomer: 0,
     });
-
-    const [tableData, setTableData] = useState([
-        { id: 1, name: 'Elizabeth Lee', company: 'AstraSystems', orderValue: 359, orderDate: '10/06/2023', status: 'New' },
-        { id: 2, name: 'Carlos Garcia', company: 'SmoozeShift', orderValue: 747, orderDate: '24/07/2023', status: 'New' },
-        { id: 3, name: 'Elizabeth Bailey', company: 'Prime Time Telecom', orderValue: 564, orderDate: '08/08/2023', status: 'In-progress' },
-        { id: 4, name: 'Ryan Brown', company: 'OmniTech Corporation', orderValue: 541, orderDate: '11/08/2023', status: 'In-progress' },
-        { id: 5, name: 'Ryan Young', company: 'DataStream Inc', orderValue: 769, orderDate: '01/09/2023', status: 'Completed' },
-        { id: 6, name: 'Hailey Adams', company: 'FlowRush', orderValue: 922, orderDate: '10/06/2023', status: 'Completed' },
-    ]);
-
     useEffect(() => {
         const fetchOverviewData = async () => {
             try {
@@ -38,27 +26,9 @@ function Admin() {
                 console.error('Error fetching overview data:', error);
             }
         };
-
-        const fetchTableData = async () => {
-            try {
-                const res = await fetch('API_URL/table-data');
-                const data = await res.json();
-                setTableData(data);
-            } catch (error) {
-                console.error('Error fetching overview data: ', error);
-            }
-        }
-
         fetchOverviewData();
-        fetchTableData();
     }, []);
-    const columns = [
-        { field: 'name', headerName: 'Customer Name', width: 150 },
-        { field: 'company', headerName: 'Company', width: 150 },
-        { field: 'orderValue', headerName: 'Order Value', width: 120, valueFormatter: ({ value }) => `$${value}` },
-        { field: 'orderDate', headerName: 'Order Date', width: 120 },
-        { field: 'status', headerName: 'Status', width: 100 },
-    ];
+
 
     return (
         <div className="container">
@@ -67,9 +37,9 @@ function Admin() {
                     <p>LOGO</p>
                 </div>
                 <div className="menu">
-                    <Link to="/" className="menu-item active">Dashboard</Link>
+                    <Link to="/" className="menu-item">Dashboard</Link>
                     <Link to="/projects" className="menu-item">Projects</Link>
-                    <Link to="/teams" className="menu-item">Teams</Link>
+                    <Link to="/teams" className="menu-item active">Teams</Link>
                     <Link to="/analytics" className="menu-item">Analytics</Link>
                     <Link to="/messages" className="menu-item">Messages</Link>
                     <Link to="/integration" className="menu-item">Integration</Link>
@@ -92,19 +62,11 @@ function Admin() {
                     </div>
                 </div>
                 <div className="content">
-                    <h3>Detailed Report</h3>
-                    <DataGrid
-                        rows={tableData}
-                        columns={columns}
-                        pageSizeOptions={[5, 10, 20]}
-                        initialState={{
-                            pagination: { paginationModel: { pageSize: 5 } },
-                        }}
-                        sortingOrder={['asc', 'desc']} />
+                    <h3>Teams</h3>
                 </div>
             </div>
         </div>
     );
 }
 
-export default Admin;
+export default Teams;
